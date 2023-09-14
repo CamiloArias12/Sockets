b5a7862630066f70ef45e0aba5194dfade075726
Jair Gaona <jairgaona60@gmail.com>
2023-09-14 14:13:46 -0500

    cambio de interfaz

--- src/view/view.java
+++ src/view/view.java
@@ -1,25 +1,9 @@
 package view;
 
-import java.awt.Color;
+public class view {
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class view extends JFrame {
-
-    JPanel componentes = new JPanel();
     public view (){
-        setSize(500, 500);
-        setLocationRelativeTo(null);
-        setLayout(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        panelComponentes();
-    }
-    public void panelComponentes(){
-        componentes.setBounds(0, 0, 500, 500);
-        componentes.setBackground(Color.BLUE);
-        add(componentes);
+
     }
 
     
